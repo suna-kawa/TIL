3917eaf4e3a00d90d7996cffa6ee4469d955a975
suna-g <sunagawa69852@gmail.com>
2022-01-22 08:49:04 +0900

    RUby

---
+++ 0122Ruby.rb
@@ -0,0 +1,20 @@
+print "hello world" # 改行されない
+puts "hello world"# 改行される
+p "hello world"  # デバック用
+
+# irb👉対話的に出力される
+# ri 👉知りたい命令やオブジェクトのドキュメントが見れる
+
+# 変数を使う際
+# _ or 英小文字からスタートしないといけない
+mth "hello hello"
+puts mth
+
+# 定数
+# 英大文字から始める
+VERSION = 1.1
+puts VERSION
+
+# objects
+# オブジェクトは便利な命令を持っているデータ型
+https://i.gyazo.com/b5633f0b70a6d357812c504615e201ae.png
