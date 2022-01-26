634f95f18f46248f1d8bac48db584d235dd69cec
suna-g <sunagawa69852@gmail.com>
2022-01-26 19:52:06 +0900

    ruby Calculation

---
+++ ゼロからわかるRuby/Calculation.rb
@@ -0,0 +1,21 @@
+# 計算する
+puts 1 * 2
+puts 3 + 7
+puts 8 / 9
+puts 8r/9r
+
+puts 3 * (5 + 9)
+
+# 累乗
+puts 7 ** 2
+
+puts 8 % 2
+
+=begin
+整数を文字列に変換
+to_s to_string(文字列へ)
+文字列を整数へ
+to_i to_integer(整数へ)
+=end
+puts 77.to_s + "45"
+puts "88".to_i + 8 
