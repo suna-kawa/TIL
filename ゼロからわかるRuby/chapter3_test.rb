f94c303199e29885893e44adac419b8ed5c07f0d
suna-g <sunagawa69852@gmail.com>
2022-02-09 23:46:06 +0900

    test part1

---
+++ ゼロからわかるRuby/chapter3_test.rb
@@ -0,0 +1,37 @@
+a = 2
+if a < 365
+  puts "aが365よりも小さい"
+elsif a == 1 + 1
+  puts "aと1+1は正しいです"
+else
+  puts "該当しません"
+end
+
+season = "春"
+unless season == "夏"
+  puts "あんまんたべたい"
+end
+
+season = "夏"
+season == "夏"
+if season == "夏" 
+  puts "かき氷たべたい"
+  puts "麦茶のみたい"
+end
+
+wallet = 100
+if wallet >= 120
+  puts "ジュースを買おう"
+else
+  puts "お金じゃ買えない幸せがたくさんあるんだ"
+end
+
+x = 200
+if x < 0 || x >= 100 
+  puts "範囲外です"
+end
+
+
+
+
+
