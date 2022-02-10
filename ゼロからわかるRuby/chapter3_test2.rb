dc9487b700089f73bdc7e7b8b6e6bf16367aecd0
suna-g <sunagawa69852@gmail.com>
2022-02-10 23:17:18 +0900

    chapter3_test2

---
+++ ゼロからわかるRuby/chapter3_test2.rb
@@ -0,0 +1,23 @@
+x = 0
+y = -1
+z = 1
+if x > 0 || y > 0 || z > 0
+  puts "正の数です"
+end
+
+season = "春"
+case season
+when "春"
+  puts "アイスを買っていこう!"
+when "夏"
+  puts "かき氷買っていこう!"
+else
+  puts "あんまんを買っていこう!"
+end
+
+2.times do
+  puts "カフェラテ" 
+  puts "モカ"
+  puts "モカ"
+end
+puts "フラペチーノ"
