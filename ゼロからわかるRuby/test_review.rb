39e8d3c0a2ca85112e003cc44fe6717f0689279b
suna-g <sunagawa69852@gmail.com>
2022-04-10 23:56:19 +0900

    add chapter2_review
    chapter2問題を復習

---
+++ ゼロからわかるRuby/test_review.rb
@@ -0,0 +1,16 @@
+# chapter2
+a = 2 + 3
+puts a
+
+b = 2 ** 2
+puts b * 3.14
+
+c = "abc"
+d = "def"
+puts c + d
+puts ["abc" , "def"].join
+
+x = {"e" => "123", "f" => "456"}
+x.each_value do |value|
+  puts value
+end
