da5501f98a51178f19175c3d6649d65313e91694
suna-g <sunagawa69852@gmail.com>
2022-04-11 23:58:56 +0900

    add test_review2
    練習問題２復習

---
+++ ゼロからわかるRuby/test_review2.rb
@@ -0,0 +1,16 @@
+a = "123"
+b = "456"
+puts a.to_i + b.to_i
+
+coffee = 400
+espre = 100
+if coffee == 300
+  puts "コーヒー:#{coffee}合計: #{coffee + espre *2}"
+else coffee == 400
+  puts "コーヒー:#{coffee}合計: #{coffee + espre *2}"
+end
+
+total = 300 * 2
+tax = total * 0.1
+puts "#{total + tax}"
+
