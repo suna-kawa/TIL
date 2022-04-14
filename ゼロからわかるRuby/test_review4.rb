8d4c1058405dde96d8a7da87df6fea48ce197c4e
suna-g <sunagawa69852@gmail.com>
2022-04-14 23:53:30 +0900

    add test_review4
    chapter4練習問題を復習

---
+++ ゼロからわかるRuby/test_review4.rb
@@ -0,0 +1,16 @@
+v = ["カフェラテ", "コーヒー"]
+p v
+
+drinks = ["コーヒー", "カフェラテ", "モカ"]
+p drinks[1]
+p drinks[0,2]
+
+drinks.unshift("キャラメルラテ")
+drinks.push("モカ")
+p drinks
+
+a = [1, 2]
+b = [3, 4]
+p [a, b].join
+
+
